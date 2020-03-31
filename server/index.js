d58644e3f478f5bcef13b3097505009caea5516f
mchalepage <me@mchale.page>
2020-03-31 17:12:42 -0600

    progress

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mc = require('./controllers/messages_controller')
+
+const app = express()
+
+app.use(express.json())
+
+const messagesBaseUrl = "/api/messages"
+app.post(messagesBaseUrl, mc.create)
+app.get(messagesBaseUrl, mc.read)
+app.put(messagesBaseUrl, mc.update)
+app.delete(messagesBaseUrl, mc.delete)
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
